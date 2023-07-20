2be8acd2c2d950f35e6aa0405f161133971612ad
Kyriel Abad <abadkyriel@gmail.com>
2023-07-21 02:55:00 +0800

    Enhance Timeutils

--- src/timeutils.ts
+++ src/timeutils.ts
@@ -2,16 +2,22 @@ import { previousSunday, addDays } from "date-fns";
 export { addDays };
 
 /**
-   * Using the current time, get the start of what we define
-   * as the "previous week" - Monday to Sunday.
-   *
-   * @returns The start of the current week, Monday.
-   */
+ * Using the current time, get the start of what we define
+ * as the "previous week" - Monday to Sunday.
+ *
+ * @returns The start of the current week, Monday.
+ */
 export function weekStart(): Date {
   const start = previousSunday(Date.now());
-  start.setHours(0);
-  start.setMinutes(0);
-  start.setSeconds(0);
-  start.setMilliseconds(0);
-  return addDays(start, 1);
-};
+  return addDays(stripTime(start), 1);
+}
+
+export function stripTime(date: Date): Date {
+  // Deep copy the original date
+  const ndate = new Date(date);
+  ndate.setHours(0);
+  ndate.setMinutes(0);
+  ndate.setSeconds(0);
+  ndate.setMilliseconds(0);
+  return ndate;
+}
